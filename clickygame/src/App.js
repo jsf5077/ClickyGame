0cf4ac556dd6b32f99d99f01099e0cdf10569ec5
jsf5077 <jsf5077@gmail.com>
2019-09-06 22:22:03 -0400

    can shuffle cards on click

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -12,6 +12,14 @@ class App extends React.Component {
     pokemon
   };
 
+  shuffle = id => {
+    this.setState({
+      pokemon: this.state.pokemon.sort(function(a, b) {
+        return 0.5 - Math.random();
+      })
+    });
+  };
+
   render() {
     return (
       <Container>
@@ -20,7 +28,7 @@ class App extends React.Component {
           <div className="col-8">
             <div className="row justify-content-center">
               {this.state.pokemon.map(pokemon => (
-              <Card id={pokemon.id} key={pokemon.id} image={pokemon.image} />
+              <Card id={pokemon.id} key={pokemon.id} image={pokemon.image} shuffle={this.shuffle}/>
               ))}
             </div>
           </div>
@@ -29,5 +37,7 @@ class App extends React.Component {
     );
   }
 }
+
+
 
 export default App;
--- clickygame/src/components/Card/index.js
+++ clickygame/src/components/Card/index.js
@@ -3,7 +3,12 @@ import "./style.css";
 
 function Card(props) {
   return (
-    <div className="card">
+    <div
+      className="card"
+      onClick={() => {
+        props.shuffle(props.id);
+      }}
+    >
       <div className="img-container">
         <img src={props.image} alt={props.name} />
       </div>
