4b05505551ea4a73d09345f05f98e6eef6288bf7
jsf5077 <jsf5077@gmail.com>
2020-03-18 02:32:32 -0400

    Updates

---
+++ precache-manifest.ed9e2877dad503c7bbd986bbf5c00517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff07904d68a6c054a83c93b2c053fa0",
+    "url": "/PokeCatch/index.html"
+  },
+  {
+    "revision": "4be90020a119e24f9f7b",
+    "url": "/PokeCatch/static/css/main.4a575afe.chunk.css"
+  },
+  {
+    "revision": "b0d9dc72b62215a246e7",
+    "url": "/PokeCatch/static/js/2.999038b7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/PokeCatch/static/js/2.999038b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be90020a119e24f9f7b",
+    "url": "/PokeCatch/static/js/main.780b3fdb.chunk.js"
+  },
+  {
+    "revision": "c2620cc63a5b20716f29",
+    "url": "/PokeCatch/static/js/runtime-main.d0683fb6.js"
+  }
+]);
